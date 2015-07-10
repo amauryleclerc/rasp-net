999ceddf1c1bf7c2003ca05fcf5bfa6172218164
Amaury LECLERC <amauryleclerc@hotmail.fr>
2015-07-10 21:13:01 +0200

    network info

--- src/main/java/fr/aleclerc/rasp/net/NetworkListener.java
+++ src/main/java/fr/aleclerc/rasp/net/NetworkListener.java
@@ -1,10 +1,13 @@
 package fr.aleclerc.rasp.net;
 
+import java.io.IOException;
+
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.system.NetworkInfo;
 
 public class NetworkListener implements NetworkListenerInterface {
 	private final GpioController gpio = GpioFactory.getInstance();
@@ -21,11 +24,30 @@ public class NetworkListener implements NetworkListenerInterface {
 		ledV.setShutdownOptions(true, PinState.LOW);
 		ledR.setShutdownOptions(true, PinState.LOW);
 		ledR.high();
+        System.out.println("----------------------------------------------------");
+        System.out.println("NETWORK INFO");
+        System.out.println("----------------------------------------------------");
+        
+        // display some of the network information
+       
+        try {
+        	 System.out.println("Hostname          :  " + NetworkInfo.getHostname());
+             for (String ipAddress : NetworkInfo.getIPAddresses())
+                 System.out.println("IP Addresses      :  " + ipAddress);
+             for (String fqdn : NetworkInfo.getFQDNs())
+                 System.out.println("FQDN              :  " + fqdn);
+			for (String nameserver : NetworkInfo.getNameservers())
+			    System.out.println("Nameserver        :  " + nameserver);
+		} catch (IOException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void sendNetworkStatus(String status) {
 		System.out.println("status "+status);
+		
 		if (status.equals("OK")) {
 			ledV.high();
 			ledR.low();
